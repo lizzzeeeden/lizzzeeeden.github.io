670864e3dfdcd95d402778e31b52549250fe83fa
lizzzeeeden <1449563241@qq.com>
2021-09-05 12:36:46 +0800

    change layout
    columns:1->2

--- script.js
+++ script.js
@@ -7,20 +7,36 @@ function copy(id) {
 
 //点击展开视频
 function togglevideo(id) {
-    v = document.getElementById(id);
-    b = document.getElementById("btn_" + id);
+    box = document.getElementById("box_" + id);
+    video = document.getElementById("video_" + id);
+    btn = document.getElementById("videobtn_" + id);
 
 
     // 第一次载入0会变成空字符串，因此用这个判断/////////////////////////把这个写到csdn上去
-    if (v.style.width != "100%") { //视频展开
-        v.style.width = "100%";
-
+    if (video.style.width != "100%") { //视频展开
+        //修改gamebox宽
+        box.style.width = "100%";
+        //修改视频大小
+        video.style.width = "100%";
         //修改按键样式
-        b.setAttribute("class", "videobtn openvideobtn");
+        btn.setAttribute("class", "videobtn openvideobtn");
     } else { //视频关闭
-        v.style.width = "0%";
+        //修改gamebox宽
+        box.style.width = "43%";
+
+        //修改视频大小
+        video.style.width = "0%";
 
         //修改按键样式
-        b.setAttribute("class", "videobtn");
+        btn.setAttribute("class", "videobtn");
+
     }
+
+    setTimeout(function () {
+        window.scrollTo({
+            top: box.offsetTop - 32,
+            behavior: 'smooth',
+        })
+        // box.scrollIntoView();
+    }, 500);
 }
